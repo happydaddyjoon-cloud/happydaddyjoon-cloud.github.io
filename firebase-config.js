44c65e8f35e0a91260931785af228d5b91cb8f89
happydaddyjoon-cloud <happydaddyjoon@gmail.com>
2026-07-24 09:11:38 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// felizpapa 게시판 Firebase 설정 (together.html, guestbook.html 공용)
+// 값을 바꾸기 전(여기에_붙여넣기)에는 '미리보기 모드'로 이 브라우저에만 저장됩니다.
+export const firebaseConfig = {
+  apiKey: "AIzaSyDt31g-x7sBv4BLmo57SNvOdlN8UeLF--M",
+  authDomain: "felizpapa-e41c7.firebaseapp.com",
+  projectId: "felizpapa-e41c7",
+  storageBucket: "felizpapa-e41c7.firebasestorage.app",
+  messagingSenderId: "644144411437",
+  appId: "1:644144411437:web:297e716ef67855d52999a4"
+};
